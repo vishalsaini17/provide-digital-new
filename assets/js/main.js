a3c1bca7c80d75d6c2ee44879e9215f4d027b965
Vishal Saini <vishal.saini@pristyncare.com>
2022-02-23 22:51:27 +0530

    design update

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,37 @@ $('#slider').cardSlider({
 //         console.log('onCurrent')
 //     }
 // })
+
+$('.slick-carousel').slick({
+    infinite: true,
+    slidesToShow: 2, // Shows a three slides at a time
+    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+    arrows: false, // Adds arrows to sides of slider
+    dots: false, // Adds the dots on the bottom
+    responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
