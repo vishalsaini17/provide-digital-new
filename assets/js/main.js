b03cf4814a35b72161c8cb2ad7e9500da334448e
Vishal Saini <vishal.saini@pristyncare.com>
2022-03-09 15:56:29 +0530

    remove slider plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,105 +1,48 @@
 
-
-$('#slider').cardSlider({
-    slideTag: 'div',
-    slideClass: 'slide',
-    current: 2,
-    followingClass: 'slider-content'
-})
-
-
-$('#slider').cardSlider({
-    slideTag: 'div',
-    slideClass: 'slide',
-    onBeforeMove: function (slider, onMove) {
-        console.log('onBeforeMove')
-        // $(this).prev().css({ "background-color": "yellow", "left": "0px", "transition": "none" });
-        onMove()
-    },
-    onMove: function (slider, onAfterMove) {
-        onAfterMove()
-    },
-    onAfterMove: function () {
-        console.log('onAfterMove')
-    },
-    onAfterTransition: function () {
-        console.log('onAfterTransition')
-    },
-    onCurrent: function () {
-        console.log('onCurrent')
-    }
-})
-
-
-// window.slider = $('#slider').cardSlider({
-//     slideTag: 'div'
-//     , slideClass: 'slide'
-//     , current: 1
-//     , followingClass: 'slider-content-6'
-//     , delay: 300
-//     , transition: 'ease'
-//     , onBeforeMove: function (slider, onMove) {
-//         console.log('onBeforeMove')
-//         onMove()
-//     }
-//     , onMove: function (slider, onAfterMove) {
-//         onAfterMove()
-//     }
-//     , onAfterMove: function () {
-//         console.log('onAfterMove')
-//     }
-//     , onAfterTransition: function () {
-//         console.log('onAfterTransition')
-//     }
-//     , onCurrent: function () {
-//         console.log('onCurrent')
-//     }
-// })
-
 $('.slick-carousel').slick({
-    infinite: true,
-    slidesToShow: 2, // Shows a three slides at a time
-    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
-    arrows: false, // Adds arrows to sides of slider
-    dots: false, // Adds the dots on the bottom
-    responsive: [
-        {
-          breakpoint: 1025,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: false,
-            dots: false
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            initialSlide: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-  });
-
-
-  wow = new WOW(
+  infinite: true,
+  slidesToShow: 2, // Shows a three slides at a time
+  slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+  arrows: false, // Adds arrows to sides of slider
+  dots: false, // Adds the dots on the bottom
+  responsive: [
     {
-    boxClass:     'wow',      // default
-    animateClass: 'rotateInDownLeft', // default
-    offset:       0,          // default
-    mobile:       true,       // default
-    live:         true        // default
-  }
-  )
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
-  wow.init();
+
+wow = new WOW(
+  {
+    boxClass: 'wow',      // default
+    animateClass: 'rotateInDownLeft', // default
+    offset: 0,          // default
+    mobile: true,       // default
+    live: true        // default
+  }
+)
+
+wow.init();
 
